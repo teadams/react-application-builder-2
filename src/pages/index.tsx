d222ec053deb2924ce0d34d8069373188a1e1639
Tracy Adams <tracy@foxyai.com>
2022-09-11 14:32:15 -0400

    starting typedoc, cell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          ACS react appbuilder
+          ACS react appbuilder ff
         </h1>
 
 
