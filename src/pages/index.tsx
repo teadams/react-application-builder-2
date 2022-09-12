aebf4fed475e9b4bd872d7c8a5c37e2e961c7d8b
Tracy Adams <tracy@foxyai.com>
2022-09-12 09:33:42 -0400

    scaffold of Cell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
+import {Cell}  from "../sudoku/components"
 
 const Home: NextPage = () => {
   return (
@@ -14,9 +14,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          ACS react appbuilder ff
+          <Cell ></Cell>
         </h1>
-
 
         <div className={styles.grid}>
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
