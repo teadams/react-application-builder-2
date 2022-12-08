56faa815519a5c68f4cdac1d5e0a627a3a5818d4
Tracy Adams <tracy@foxyai.com>
2022-12-08 15:09:30 -0500

    bug fix - inserting dataSet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
 import React from "react";
+import { QueryClient, QueryClientProvider, QueryFunction } from "react-query";
 import AcsMetaContextProvider from "./contextProviders/AcsMetaContextProvider";
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+  QueryFunctionContext,
+} from "react-query";
+
+const getObjectType = (queryFunctionContext: QueryFunctionContext) => {
+  const { queryKey } = queryFunctionContext;
+  console.log(queryKey);
+};
+
+const TestQuery = (props: { objectType: string }) => {
+  const { objectType } = props;
+  const { data } = useQuery([objectType], getObjectType);
+};
+
+// Write getObject
+// Look up parateters to use query
+// Decide on the parameters to getRemote
+//    ojbectType, id, get
+
+const queryClient = new QueryClient();
+
 function AcsApp(props) {
-  return <AcsMetaContextProvider>{props.children}</AcsMetaContextProvider>;
+  return (
+    <AcsMetaContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <TestQuery />
+        {props.children}
+      </QueryClientProvider>
+    </AcsMetaContextProvider>
+  );
 }
 
 export default AcsApp;
