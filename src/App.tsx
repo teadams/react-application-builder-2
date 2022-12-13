398561be06ef012566da93671acb40978e884457
Tracy Adams <tracy@foxyai.com>
2022-12-13 12:54:48 -0500

    first link of react-query and next

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,6 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { AcsMetaContextProvider } from "./contextProviders";
-import { useGetObjectDataById } from "./hooks";
-
-const TestQuery = (props: { objectType: string }) => {
-  const { objectType } = props;
-  const { data } = useGetObjectDataById(
-    objectType,
-    "a518aa59-8256-41e3-b4df-51de10cd74df"
-  );
-  console.log(data);
-};
 
 const queryClient = new QueryClient();
 
@@ -21,7 +11,6 @@ function AcsApp(props) {
   return (
     <AcsMetaContextProvider>
       <QueryClientProvider client={queryClient}>
-        <TestQuery objectType="jobStages" />
         {props.children}
       </QueryClientProvider>
     </AcsMetaContextProvider>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,17 +1,16 @@
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
-import { QueryFunctionContext } from "react-query";
 import * as api from "./api";
 
 export const getObjectData = async (
-  queryFunctionContext: QueryFunctionContext
-) => {
-  const { queryKey } = queryFunctionContext;
-  const [objectType, filters] = queryKey;
+  objectType: string,
+  filters: string[] = []
+): Promise<unknown> => {
   const path = "acs/" + objectType;
-  console.log(filters);
   const apiResult = await api.callAPI({ path, params: filters });
   console.log(apiResult);
+
+  return apiResult;
 };
 
 export default { getObjectData };
