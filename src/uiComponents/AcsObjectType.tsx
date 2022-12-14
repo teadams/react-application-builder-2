691f33801039038a46bfbb0a8ed6f3e51d01113e
Tracy Adams <tracy@foxyai.com>
2022-12-14 14:45:35 -0500

    moving to greetr for now

--- src/uiComponents/AcsObjectType.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useGetObjectTypeData } from "../hooks";
-
-export const AcsObjectType = ({ objectType }: { objectType: string }) => {
-  const { data } = useGetObjectTypeData(objectType);
-  return <>{JSON.stringify(data)} </>;
-};
-
-export default AcsObjectType;
--- src/uiComponents/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./AcsObject";
-export * from "./AcsObjectType";
