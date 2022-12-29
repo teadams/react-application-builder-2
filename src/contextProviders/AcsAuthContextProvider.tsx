e223af8ad6414484d3801230194527c98edc62a9
Tracy Adams <tracy@foxyai.com>
2022-12-28 21:22:27 -0500

    addin context

--- src/contextProviders/AcsAuthContextProvider.tsx
+++ src/contextProviders/AcsAuthContextProvider.tsx
@@ -7,21 +7,25 @@ export const AcsAuthContextProvider = (props: any) => {
   const [userId, setUserId] = useState("");
 
   return (
-    <AcsAuthContext.Provider value={{
-      userId: userId,
-      user: user,
-      logout: () => {
-        localStorage.removeItem("user");
-        setUser({});
-        setUserId("");
-      },
-      login: (acsUserAuthContext) => {
-        localStorage.setItem("user", JSON.stringify(acsUserAuthContext.jwtToken));
-        setUserId(acsUserAuthContext.id as string)
-        setUser(acsUserAuthContext.user as {[key:string]: unknown})
-      }
-    }}
- 
+    <AcsAuthContext.Provider
+      value={{
+        userId: userId,
+        user: user,
+        logout: () => {
+          localStorage.removeItem("user");
+          setUser({});
+          setUserId("");
+        },
+        login: (acsUserAuthContext) => {
+          localStorage.setItem(
+            "user",
+            JSON.stringify(acsUserAuthContext.jwtToken)
+          );
+          setUserId(acsUserAuthContext.id as string);
+          setUser(acsUserAuthContext.user as { [key: string]: unknown });
+        },
+      }}
+    >
       {props.children}
     </AcsAuthContext.Provider>
   );
