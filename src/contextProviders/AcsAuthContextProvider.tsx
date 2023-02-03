58c00bdfcff28fc1f376f57dd44f3279b67a1aa6
Tracy Adams <tracyeadams@gmail.com>
2023-02-02 23:38:48 -0500

    merging

--- src/contextProviders/AcsAuthContextProvider.tsx
+++ src/contextProviders/AcsAuthContextProvider.tsx
@@ -6,6 +6,7 @@ import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 export const AcsAuthContextProvider = (props: any) => {
   const [user, setUser] = useState({});
   const [userId, setUserId] = useState("");
+  const [userInfo, setUserInfo] = useState({});
 
   const tenantSetup: { [index: string]: unknown } = {};
   const { data = [] } = useGetData("acsTenantSetup");
@@ -13,24 +14,45 @@ export const AcsAuthContextProvider = (props: any) => {
     tenantSetup[row.parameter as string] = row.value;
   }
 
+  useEffect(() => {
+    const userFromLocalstorage = localStorage.getItem("user");
+    const userInfoFromLocalstorage = localStorage.getItem("userInfo");
+    if (userFromLocalstorage !== "undefined") {
+      const user = JSON.parse(userFromLocalstorage as string);
+      setUser(user);
+    }
+    if (userInfoFromLocalstorage !== "undefined") {
+      const userInfo = JSON.parse(userInfoFromLocalstorage as string);
+      setUserInfo(userInfo);
+    }
+  }, []);
+
   return (
     <AcsAuthContext.Provider
       value={{
         userId: userId,
         user: user,
         tenantSetup: tenantSetup,
+        userInfo: userInfo,
         logout: () => {
           localStorage.removeItem("user");
+          localStorage.removeItem("userInfo");
           setUser({});
           setUserId("");
+          setUserInfo({});
         },
         login: (acsUserAuthContext) => {
           localStorage.setItem(
             "user",
             JSON.stringify(acsUserAuthContext.jwtToken)
           );
+          localStorage.setItem(
+            "userInfo",
+            JSON.stringify(acsUserAuthContext.user)
+          );
           setUserId(acsUserAuthContext.id as string);
           setUser(acsUserAuthContext.user as { [key: string]: unknown });
+          setUserInfo(acsUserAuthContext.user as object);
         },
       }}
     >
