49f6668fb3c2735196bdd9fbce36911ca434b061
Tracy Adams <tracyeadams@gmail.com>
2023-02-05 17:06:18 -0500

    vercel

--- src/uiComponents/AcsObjectType.tsx
+++ src/uiComponents/AcsObjectType.tsx
@@ -4,25 +4,27 @@ import AcsDataTable from "./AcsDataTable";
 import { fieldsForObjectType } from "../lib/acsDataTableParams";
 
 export const AcsObjectType = ({ objectType = "" }: { objectType: string }) => {
-  const objectTypeMeta = useGetAcsMetaObjectType(objectType);
-  const { data } = useGetData(objectType);
-  if (!data || !objectTypeMeta || !objectType) {
-    return null;
-  }
+	const objectTypeMeta = useGetAcsMetaObjectType(objectType);
+	const { data } = useGetData(objectType);
+	if (!data || !objectTypeMeta || !objectType) {
+		return null;
+	}
 
-  const objectTypeFields = objectTypeMeta.fields;
+	const objectTypeFields = objectTypeMeta.fields;
 
-  return (
-    <div>
-      <AcsDataTable
-        data={data}
-        title={objectTypeMeta?.prettyPlural}
-        objectTypeFields={objectTypeFields}
-        objectType={objectType}
-        fieldsToDisplay={fieldsForObjectType[objectType]}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<AcsDataTable
+				data={data}
+				title={objectTypeMeta?.prettyPlural}
+				objectTypeFields={objectTypeFields}
+				objectType={objectType}
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				fieldsToDisplay={fieldsForObjectType[objectType]}
+			/>
+		</div>
+	);
 };
 
 export default AcsObjectType;
