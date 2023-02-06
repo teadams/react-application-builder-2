378b22c25bd3b73013175af70e1fdf61c4cb22bb
summaiya <summaiya.shoro@lathran.com>
2023-02-06 13:28:16 +0500

    fix: typescript errors fix for build

--- src/lib/acsDataTableParams.ts
+++ src/lib/acsDataTableParams.ts
@@ -1,4 +1,4 @@
-export const fieldsForObjectType: object = {
+export const fieldsForObjectType: Record<string, any> = {
     jobs: ["name", "departments", "status", "offices", "hiring_managers","recruiters","job_posting_urls","createdAt"],
     candidates: ["first_name","last_name","company","title","email_addresses","applications","createdAt"],
     applications: ["job","current_stage","applied_at","rejected_at","source","candidate","credited_to","createdAt"],
