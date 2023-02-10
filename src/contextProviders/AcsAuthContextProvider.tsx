e77c32616e0e39b35d4809b2f506e806a7610ad8
Tracy Adams <tracyeadams@gmail.com>
2023-02-09 22:33:31 -0500

    null data

--- src/contextProviders/AcsAuthContextProvider.tsx
+++ src/contextProviders/AcsAuthContextProvider.tsx
@@ -4,61 +4,62 @@ import { useGetData } from "../hooks";
 import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 export const AcsAuthContextProvider = (props: any) => {
-  const [user, setUser] = useState({});
-  const [userId, setUserId] = useState("");
-  const [userInfo, setUserInfo] = useState({});
+	const [user, setUser] = useState({});
+	const [userId, setUserId] = useState("");
+	const [userInfo, setUserInfo] = useState({});
 
-  const tenantSetup: { [index: string]: unknown } = {};
-  const { data = [] } = useGetData("acsTenantSetup");
-  for (const row of data) {
-    tenantSetup[row.parameter as string] = row.value;
-  }
+	const tenantSetup: { [index: string]: unknown } = {};
+	const { data = [] } = useGetData("acsTenantSetup");
+	console.log(data);
+	for (const row of data ?? []) {
+		tenantSetup[row.parameter as string] = row.value;
+	}
 
-  useEffect(() => {
-    const userFromLocalstorage = localStorage.getItem("user");
-    const userInfoFromLocalstorage = localStorage.getItem("userInfo");
-    if (userFromLocalstorage !== "undefined") {
-      const user = JSON.parse(userFromLocalstorage as string);
-      setUser(user);
-    }
-    if (userInfoFromLocalstorage !== "undefined") {
-      const userInfo = JSON.parse(userInfoFromLocalstorage as string);
-      setUserInfo(userInfo);
-    }
-  }, []);
+	useEffect(() => {
+		const userFromLocalstorage = localStorage.getItem("user");
+		const userInfoFromLocalstorage = localStorage.getItem("userInfo");
+		if (userFromLocalstorage !== "undefined") {
+			const user = JSON.parse(userFromLocalstorage as string);
+			setUser(user);
+		}
+		if (userInfoFromLocalstorage !== "undefined") {
+			const userInfo = JSON.parse(userInfoFromLocalstorage as string);
+			setUserInfo(userInfo);
+		}
+	}, []);
 
-  return (
-    <AcsAuthContext.Provider
-      value={{
-        userId: userId,
-        user: user,
-        tenantSetup: tenantSetup,
-        userInfo: userInfo,
-        logout: () => {
-          localStorage.removeItem("user");
-          localStorage.removeItem("userInfo");
-          setUser({});
-          setUserId("");
-          setUserInfo({});
-        },
-        login: (acsUserAuthContext) => {
-          localStorage.setItem(
-            "user",
-            JSON.stringify(acsUserAuthContext.jwtToken)
-          );
-          localStorage.setItem(
-            "userInfo",
-            JSON.stringify(acsUserAuthContext.user)
-          );
-          setUserId(acsUserAuthContext.id as string);
-          setUser(acsUserAuthContext.user as { [key: string]: unknown });
-          setUserInfo(acsUserAuthContext.user as object);
-        },
-      }}
-    >
-      {props.children}
-    </AcsAuthContext.Provider>
-  );
+	return (
+		<AcsAuthContext.Provider
+			value={{
+				userId: userId,
+				user: user,
+				tenantSetup: tenantSetup,
+				userInfo: userInfo,
+				logout: () => {
+					localStorage.removeItem("user");
+					localStorage.removeItem("userInfo");
+					setUser({});
+					setUserId("");
+					setUserInfo({});
+				},
+				login: (acsUserAuthContext) => {
+					localStorage.setItem(
+						"user",
+						JSON.stringify(acsUserAuthContext.jwtToken)
+					);
+					localStorage.setItem(
+						"userInfo",
+						JSON.stringify(acsUserAuthContext.user)
+					);
+					setUserId(acsUserAuthContext.id as string);
+					setUser(acsUserAuthContext.user as { [key: string]: unknown });
+					setUserInfo(acsUserAuthContext.user as object);
+				},
+			}}
+		>
+			{props.children}
+		</AcsAuthContext.Provider>
+	);
 };
 
 export default AcsAuthContextProvider;
