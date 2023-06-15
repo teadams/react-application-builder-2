e13892b923ad5bdd22bf26a8a257e87b133e2b76
Tracy Adams <tracyeadams@gmail.com>
2023-06-15 12:49:11 -0400

    adding domain parameter

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,16 +4,15 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { getServerDomain } from "../../../lib/acsHooks";
 import { hostname } from "os";
-
 type APIMethod = "GET" | "POST" | "PUT" | "DELETE";
 type ACSApiParam = "acsCount" | "acsMax" | unknown;
 interface API {
+  domain?: string;
   path?: string;
   params?: { [index: string]: unknown };
   data?: { [index: string]: unknown };
   method?: APIMethod;
 }
-
 const getHeaders = () => {
   const user = localStorage.getItem("user");
   let authHeader = {};
@@ -23,16 +22,13 @@ const getHeaders = () => {
   }
   return authHeader;
 };
-
 export const getDomain = () => {
   console.log("get domain");
   console.log("env is " + process.env.NEXT_PUBLIC_API_LOCATION);
-
   return process.env.NEXT_PUBLIC_API_LOCATION
     ? process.env.NEXT_PUBLIC_API_LOCATION
     : getServerDomainFromHostname();
 };
-
 export const getHostname = () => {
   return process.env.NEXT_PUBLIC_DOMAIN
     ? process.env.NEXT_PUBLIC_DOMAIN
@@ -40,7 +36,6 @@ export const getHostname = () => {
     ? window.location.hostname
     : "localhost";
 };
-
 export const getServerDomainFromHostname = () => {
   console.log("get server domain from hosting");
   const serverDomain = getServerDomain();
@@ -49,7 +44,6 @@ export const getServerDomainFromHostname = () => {
     "PRoess env next_public Domain " + process.env.NEXT_PUBLIC_DOMAIN
   );
   // console.log("window location " + window.location.hostname);
-
   const hostname = getHostname();
   if (hostname === "localhost") {
     return "http://localhost:2000";
@@ -65,15 +59,14 @@ export const getServerDomainFromHostname = () => {
     .join(".")}`;
   return finalHostname;
 };
-
 export async function callAPI({
+  domain = "",
   path = "",
   params = {},
   data = {},
   method = "GET",
 }: API): Promise<unknown> {
   console.log("calling api " + path);
-
   // if (data_object) {
   //   Generically handles file uploads,which requires special handling */
   //   let multi_object = new FormData();
@@ -104,17 +97,14 @@ export async function callAPI({
   // if (jwt_token) {
   //   auth_header = { "x-access-token": jwt_token };
   // }
-
-  const domain = getDomain();
+  // const domain = getDomain();
   console.log("DOMAIn IS " + domain);
   console.log("PATH IS " + path);
   // Temp until dev environments are hooked together
-  if (!domain) return null;
+  if (!domain) domain = getDomain();
   // TODO: params
-
   console.log("getting from ");
   console.log(`${domain}/${path}`);
-
   const apiResult = await axios({
     method: method,
     url: `${domain}/${path}`,
@@ -125,11 +115,10 @@ export async function callAPI({
     const paramStr = JSON.stringify(params);
     const dataStr = JSON.stringify(data);
     const error_prompt = `error connecting to server with url: ${domain}/${path} method: ${method}
-     params: ${paramStr} 
+     params: ${paramStr}
      data: ${dataStr}`;
     // alert(error_prompt + error.message + " " + error.stack);
     console.log("error", error);
-
     toast.error(error.message?.toString(), {
       className: "text-sm",
     });
