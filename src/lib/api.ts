b52271488258e8e926d1295b4649c6fd2c1d1362
Tracy Adams <tracyeadams@gmail.com>
2023-11-27 15:26:22 -0500

    error handling

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -113,7 +113,6 @@ export const getServerDomainFromHostname = () => {
     }
   }
   
-    console.log("final hostname is " + splicedHostname)
     const finalHostname = `https://${splicedHostname.concat(serverDomain).join(".")}`;
     return finalHostname;
 };
@@ -126,78 +125,47 @@ export async function callAPI({
   method = "GET",
 }: API): Promise<unknown> {
   console.log("call api")
-  // if (data_object) {
-  //   Generically handles file uploads,which requires special handling */
-  //   let multi_object = new FormData();
-  //   let acs_type_protected_fields = {};
-  //   Object.keys(data_object).forEach((key) => {
-  //     if (
-  //       typeof data_object[key] === "file" ||
-  //       data_object[key] instanceof Blob ||
-  //       typeof data_object[key] === "string"
-  //     ) {
-  //       multi_object.append(key, data_object[key]);
-  //     } else {
-  //       acs_type_protected_fields[key] = data_object[key];
-  //     }
-  //   });
-  //   if (acs_type_protected_fields) {
-  //     // FormData always sends strings. So to protect booleans, numbers, etc.
-  //     // we will stringify the non-file attributes and parse server side.
-  //     multi_object.append(
-  //       "acs_type_protected_fields",
-  //       JSON.stringify(acs_type_protected_fields)
-  //     );
-  //   }
-  //   data_object = multi_object;
-  // }
+ 
 
   if (!domain) domain = getDomain();
 
   const headers = getHeaders()
+  try {
+    const apiResult = await axios({
+      method: method,
+      url: `${domain}/${path}`,
+      data: data,
+      params: params,
+      headers,
+    })
 
-  const apiResult = await axios({
-    method: method,
-    url: `${domain}/${path}`,
-    data: data,
-    params: params,
-    headers,
-  }).catch((error) => {
-    console.log("ERROR in calling api");
-    const paramStr = JSON.stringify(params);
-    const dataStr = JSON.stringify(data);
-    const error_prompt = `error connecting to server with url: ${domain}/${path} method: ${method}
-     params: ${paramStr}
-     data: ${dataStr}`;
-    // alert(error_prompt + error.message + " " + error.stack);
-    console.log("error", error);
-    toast.error(error.message?.toString(), {
+    if (apiResult) {
+      let data = apiResult.data
+      if (apiResult.data.status === "error") {
+        // The login is incorrect
+        // Log the user out and retry without login
+        if (apiResult.data.authorization_errors) {
+          if (typeof window !== "undefined") {
+            localStorage.removeItem("user");
+            localStorage.removeItem("userInfo");
+            //** Authorization failed. Call the same API with no username */
+            return callAPI({ path, params, data, method });
+          }
+        } else if (apiResult.status !== 200) {
+   
+            toast.error(apiResult.data.message, {
+              className: "text-sm",
+            });
+    
+        }
+        data = {};
+      }  
+        return data;
+    
+    }
+  } catch (error) { 
+    toast.error(error.message, {
       className: "text-sm",
     });
-    return { status: error };
-  });
-  // TODO user friendly correct error handling
-  if (apiResult) {
-    if (apiResult.data.status === "error") {
-      if (apiResult.data.validation_errors) {
-        //u.a("Validation Error", apiResult.data.validation_errors);
-      }
-      // The login is incorrect
-      // Log the user out and retry without login
-      if (apiResult.data.authorization_errors) {
-        if (typeof window !== "undefined") {
-          localStorage.removeItem("user");
-					localStorage.removeItem("userInfo");
-          //** Authorization failed. Call the same API with no username */
-          return callAPI({ path, params, data, method });
-        }
-      }
-      data = {};
-    } else {
-      console.log(apiResult.data)
-
-      data = apiResult.data;
-    }
   }
-  return data;
 }
