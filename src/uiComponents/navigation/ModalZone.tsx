0816b23f97c28c70f21ec60a213b99256d686770
Tracy Adams <tracyeadams@gmail.com>
2024-06-11 20:49:05 -0400

    classname

--- src/uiComponents/navigation/ModalZone.tsx
+++ src/uiComponents/navigation/ModalZone.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Modal } from "../layout/Modal"
 import { on } from "events";
 
-const ModalZone = ({ modalComponent, modalProps, title, children }:
-	{ modalComponent: any, modalProps: Record<string, unknown>, title?: string, children?: React.ReactNode }) => {
+const ModalZone = ({ modalComponent, modalProps, title,
+	className = "flex flex-col w-full justify-start items-center",
+	children }:
+	{ modalComponent: any, modalProps: Record<string, unknown>, title?: string, className?: string, children?: React.ReactNode }) => {
 	const [showModal, setShowModal] = React.useState(false);
 
 	const ModalComponent = modalComponent
 
 	return (
 		<>
-			<div onClick={() => setShowModal(true)}>{children}</div>
+			<div className={className}
+				onClick={() => setShowModal(true)}>{children}</div >
 			{showModal && <Modal title={title} closeModal={() => setShowModal(false)}>
 				{ModalComponent && <ModalComponent {...modalProps} closeModal={() => setShowModal(false)} />}
-			</Modal>}
+			</Modal>
+			}
 		</>
 	)
 }
